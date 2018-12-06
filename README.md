42cd476e0bba3059248f310b3a3c9e9e2dff42eb
haradama <harada@ht.sfc.keio.ac.jp>
2018-12-06 10:43:28 +0900

    add logo and tests

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+![pHash](./logo.png)
 # pHash
+
+## Usage
+
+## License
+
+[GNU General Public License v3.0](https://github.com/haradama/pHash/blob/master/LICENSE)
---
+++ tests/install_test_data.sh
@@ -0,0 +1,8 @@
+ACCESSIONs=(NZ_CP007509 NZ_CP007510 NZ_CP008957 NZ_CP008958)
+
+mkdir tmp
+for ACCESSION in ${ACCESSIONs[@]}; do
+    curl -L "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=$ACCESSION&rettype=fasta&retmode=text" > ./tmp/$ACCESSION.fna
+done
+
+cat ./tmp/*.fna > testData.fna
